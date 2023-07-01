371411135ea0bf066245efada73a59194ccbbcff
THEINHNAM\theinhnam <iamnguyenduythanhnam@gmail.com>
2023-07-01 18:00:14 +0700

    update views

---
+++ Manage_Perfume_Shop/src/Application/Application.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Application;
+
+import Views.LoginJFrame;
+
+/**
+ *
+ * @author theinhnam
+ */
+public class Application {
+    public static void main(String[] args) {
+        new LoginJFrame().setVisible(true);
+    }
+}
